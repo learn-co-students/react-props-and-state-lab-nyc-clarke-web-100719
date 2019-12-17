a50b942a02dd2ff4f7b5a96c86a2247f90eee928
Jeffrey Paulino <jp7kode@gmail.com>
2019-12-17 14:27:01 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -11,11 +11,51 @@ class App extends React.Component {
       pets: [],
       filters: {
         type: 'all'
-      }
+      },
+      adoptedPets: []
     }
   }
 
+  onChangeType = (event) => {
+    this.setState({
+      filters:{
+        type: event.target.value
+      },
+      pets: []
+    });
+
+    console.log(this.state.filters.type);
+  };
+
+  onFindPetsClick = () => {
+    let type = this.state.filters.type !== "all"
+                  ? `?type=${this.state.filters.type}`
+                  : "";
+
+
+    fetch(`/api/pets${type}`)
+      .then(data => data.json())
+      .then(data => this.setState({pets: [...data]}))
+      .catch(console.error);
+  };
+
+  onAdoptPet = (petID) => {
+    const pets = this.state.pets
+      .map(pet => pet.id === petID
+        ? {...pet, isAdopted: true}
+        : pet
+      );
+
+    console.log(pets);
+
+    this.setState({
+      pets: pets
+    });
+  };
+
   render() {
+    // this.onFindPetsClick();
+
     return (
       <div className="ui container">
         <header>
@@ -24,10 +64,16 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType}
+                       onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser
+                // pets={this.state.pets.length !== 0 ? this.state.pets : null}
+                pets={this.state.pets}
+                onAdoptPet={this.onAdoptPet}
+              />
             </div>
           </div>
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React from 'react';
+import fetchMock from "fetch-mock";
+import {getByType} from "../data/pets";
+import PropTypes from 'prop-types';
 
+// import fetchMock from ''
 class Filters extends React.Component {
+
+  static propTypes = {
+    onChangeType: PropTypes.func,
+    onFindPetsClick: PropTypes.func
+  };
+
+  // static defaultProps = {
+  //   onChangeType: console.log("empty onChangeType function props"),
+  //   onFindPetsClick: console.log("empty onFindPetsClick function props")
+  // };
+
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange={this.props.onChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +30,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     )
