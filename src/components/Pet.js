45b1973229a095a08920b25a22defeffb8209c07
github username <github email address>
2019-12-17 17:46:22 -0500

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,29 +1,31 @@
 import React from 'react'
 
-class Pet extends React.Component {
-  render() {
+const Pet = (props)=> {
+  
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {/*'♀' OR '♂' */props.gender}
+            {props.name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{props.typePet}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {props.age}</p>
+            <p>Weight: {props.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {props.isAdopted ? 
+          <button className="ui disabled button">Already adopted</button> :
+          <button className="ui primary button" onClick={()=>props.onAdoptPet(props.id)}>Adopt pet</button>
+          }
         </div>
       </div>
     )
-  }
+  
 }
 
 export default Pet
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,10 +2,31 @@ import React from 'react'
 
 import Pet from './Pet'
 
-class PetBrowser extends React.Component {
-  render() {
-    return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+const PetBrowser = (props) => {
+  let renderPets = ()=>{
+    return props.pets.map(pet => 
+      {
+        return <Pet 
+          pet={pet}
+          key={pet.id}
+          id={pet.id}
+          name={pet.name}
+          age={pet.age}
+          weight={pet.weight}
+          gender={pet.gender==="male" ? '♂' : '♀'}
+          typePet={pet.type}
+          isAdopted={pet.isAdopted}
+          onAdoptPet={props.onAdoptPet}
+        />
+      })
   }
+    return (<div className="ui cards">{renderPets()}</div>)
 }
-
+// //id: '5c142d9e-ea45-4231-8146-cccf71c704c0',
+// type: 'dog',
+// gender: 'male',
+// age: 4,
+// weight: 1,
+// name: 'Trident',
+// isAdopted: false,
 export default PetBrowser
